c2d64acbdcf5b3a685ab1ce4faa6f35cb61e0a1f
meetariesduyon <ard4496@students.uc-bcf.edu.ph>
2025-06-30 22:36:26 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+}
+
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Thank you for your message!');
+});
